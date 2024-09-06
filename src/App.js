a1e998e4237a39a4b1b48f1ab2789f5ea0bbb9d3
Miguelit023 <152641282+Miguelit023@users.noreply.github.com>
2024-09-05 23:58:35 -0600

    Add files via upload
    ARCHIVOS_TAREA

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="header">
+
+      <nav className='bar_menu'>
+
+        <h1 className='title'>PACHUCA SAN MARCOS</h1>
+        <ul className='menu'>
+          <li>MENU*</li>
+          <li><a>Videos</a></li>
+          <li><a>Costos</a></li>
+          <li><a>Uniformes</a></li>
+          <li><a>Contacto</a></li>
+        </ul>
+      </nav>
+
+
+
+    </div>
+
+
+
+
+  );
+}
+
+export default App;
